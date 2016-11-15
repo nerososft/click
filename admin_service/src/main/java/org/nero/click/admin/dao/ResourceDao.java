046abc2472e0c8f8239ed4594e5081c49fc43a85
nerosoft <nerososft@outlook.com>
2016-11-15 23:52:30 +0800

    properties

--- admin_service/src/main/java/org/nero/click/admin/dao/ResourceDao.java
+++ admin_service/src/main/java/org/nero/click/admin/dao/ResourceDao.java
@@ -6,6 +6,8 @@ import org.click.admin.entity.Resource;
 import java.util.List;
 
 /**
+ * 资源相关数据库操作
+ *
  * Author :  root
  * Email  :  nerosoft@outlook.com
  * Date   :  16-11-15
@@ -15,6 +17,7 @@ public interface ResourceDao {
 
     /**
      * 根据哈希码查找资源
+     *
      * @param md5Code 哈希校验码
      * @return 资源
      */
@@ -22,19 +25,21 @@ public interface ResourceDao {
 
 
     /**
-     * 添加资源
+     * 添加资源记录
+     *
      * @param fileName 资源名称
-     * @param filePath 资源路劲
-     * @param hashCode 哈希检验码
-     * @param fileSize 资源大小
-     * @param fileFormat 资源格式
-     * @param uploaderId 上穿者ID
+     * @param filePath 资源路劲 64位随机串接当前毫秒时间戳哈希，注：无文件后缀
+     * @param hashCode 哈希检验码 文件byte[]哈希
+     * @param fileSize 资源大小 byte 位数
+     * @param fileFormat 资源格式  文件后缀，根据文件头判断,详见 org.nero.click.utils.file.FileType
+     * @param uploaderId 上传者ID
      * @return 0 or resourceId
      */
     int addResource(String fileName,String filePath,String hashCode,String fileSize,String fileFormat,long uploaderId);
 
     /**
      * 删除（屏蔽）资源通过资源ID
+     *
      * @param resourceId 资源id
      * @return 0 or resourceId
      */
@@ -42,6 +47,7 @@ public interface ResourceDao {
 
     /**
      * 删除（屏蔽）资源通过哈希校验码
+     *
      * @param md5Code 哈希校验码
      * @return 0 or resourceId
      */
@@ -50,6 +56,7 @@ public interface ResourceDao {
 
     /**
      * 获取资源
+     *
      * @param resourceId 资源id
      * @return 资源 or null
      */
@@ -57,6 +64,7 @@ public interface ResourceDao {
 
     /**
      * 通过校验码获取资源
+     *
      * @param hashCode 校验码
      * @return 资源列表 or null
      */
@@ -65,6 +73,7 @@ public interface ResourceDao {
 
     /**
      * 通过资源格式获取资源
+     *
      * @param type 资源格式
      * @return 资源列比 or null
      */
@@ -72,6 +81,7 @@ public interface ResourceDao {
 
     /**
      * 获取所有资源
+     *
      * @return 资源列表 or null
      */
     List<Resource> getResource();
